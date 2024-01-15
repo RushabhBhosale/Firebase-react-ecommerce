ff1f1451f451cce198371341e4cb40b056b85235
RushabhBhosale <145191870+RushabhBhosale@users.noreply.github.com>
2024-01-15 18:05:12 +0530

    Updates

--- firebase-ecommerce-app/src/components/UI/ProductCard.jsx
+++ firebase-ecommerce-app/src/components/UI/ProductCard.jsx
@@ -7,13 +7,13 @@ import { Link } from 'react-router-dom'
 const ProductCard = ({ item }) => {
    return (
       <Col lg='3' md='4' className='product'>
-         <div className="product-item">
+         <div className="product-item position-relative">
             <div className="product-img">
                <motion.img whileHover={{ scale: '0.9' }} src={item.imgUrl} alt="product" />
             </div>
             <div className="p-2 product-info">
                <h3 className='product-name'><Link to={`shop/${item.id}`}>{item.productName}</Link></h3>
-               <span>{item.category}</span>
+               <span className='product-category'>{item.category}</span>
             </div>
             <div className="product-card-bottom d-flex align-content-center justify-content-between p-2">
                <span className="price">${item.price}</span>
